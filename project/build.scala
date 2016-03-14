24568c97b1c527bf1ad2ce2169f5f60df8551877
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2016-03-14 14:36:33 +0200

    Add runner for Heroku

--- project/build.scala
+++ project/build.scala
@@ -5,7 +5,7 @@ import org.scalatra.sbt.PluginKeys._
 import com.earldouglas.xwp.JettyPlugin
 import com.mojolly.scalate.ScalatePlugin._
 import ScalateKeys._
-import com.typesafe.sbt.packager.archetypes.JavaServerAppPackaging
+import com.typesafe.sbt.packager.archetypes.{JavaAppPackaging, JavaServerAppPackaging}
 
 object Digiroad2TnitsBuild extends Build {
   val Organization = "fi.liikennevirasto.digiroad2"
@@ -29,7 +29,7 @@ object Digiroad2TnitsBuild extends Build {
         "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.1.5" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "compile;container",
         "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
@@ -45,5 +45,5 @@ object Digiroad2TnitsBuild extends Build {
         )
       }
     )
-  ).enablePlugins(JettyPlugin, JavaServerAppPackaging)
+  ).enablePlugins(JettyPlugin, JavaAppPackaging)
 }
