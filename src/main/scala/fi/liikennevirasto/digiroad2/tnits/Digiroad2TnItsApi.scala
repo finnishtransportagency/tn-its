2f21b3a530da2eecf0e7ef6289717252e35d186f
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2016-03-18 15:58:07 +0200

    Cleanup

--- src/main/scala/fi/liikennevirasto/digiroad2/tnits/Digiroad2TnItsApi.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/tnits/Digiroad2TnItsApi.scala
@@ -1,10 +1,10 @@
 package fi.liikennevirasto.digiroad2.tnits
 
-import java.net.URL
-import java.nio.file.FileSystems
+import java.net.URLEncoder
 
 import org.scalatra._
-import collection.JavaConverters._
+
+import scala.collection.JavaConverters._
 
 class Digiroad2TnItsApi extends ScalatraServlet {
 
@@ -20,12 +20,12 @@ class Digiroad2TnItsApi extends ScalatraServlet {
       </rst:ROSATTERestDatasetRefList>
     }.getOrElse {
       val dataSetUrls = dataSetIds.map { dataSetId =>
-        "http://localhost:8080/rosattedownload/download/readDataSet?dataSetID=" + java.net.URLEncoder.encode(dataSetId, "UTF-8")
+        "http://localhost:8080/rosattedownload/download/readDataSet?dataSetID=" + URLEncoder.encode(dataSetId, "UTF-8")
       }
 
       <rst:ROSATTERestDatasetRefList xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:rst="http://www.ertico.com/en/subprojects/rosatte/rst" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
         {
-        dataSetUrls.map { url => <rst:ROSATTERestDatasetRef xlink:href={ url }/> }
+          dataSetUrls.map { url => <rst:ROSATTERestDatasetRef xlink:href={ url }/> }
         }
       </rst:ROSATTERestDatasetRefList>
     }
