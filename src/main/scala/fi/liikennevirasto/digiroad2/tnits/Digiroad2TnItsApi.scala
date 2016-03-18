57d8b0e5f3da338b19746805d8603ab07327bbab
Mikko Hautala <mikko.hautala@reaktor.fi>
2016-03-18 14:44:08 +0200

    Return 404 if data set not found

--- src/main/scala/fi/liikennevirasto/digiroad2/tnits/Digiroad2TnItsApi.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/tnits/Digiroad2TnItsApi.scala
@@ -3,11 +3,9 @@ package fi.liikennevirasto.digiroad2.tnits
 import org.scalatra._
 
 class Digiroad2TnItsApi extends ScalatraServlet {
-  before() {
-    contentType = "application/xml"
-  }
 
   get("/rosattedownload/download/queryDataSets") {
+    contentType = "application/xml"
     params.get("lastValidDatasetId").map { lastValidDatasetId =>
       <rst:ROSATTERestDatasetRefList xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:rst="http://www.ertico.com/en/subprojects/rosatte/rst" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
         <rst:ROSATTERestDatasetRef xlink:href="/rosattedownload/download/readDataSet?dataSetID=1uShiYqi%2fEe120s9P1ga7AAAAN2DTdVBAAAAcIdN1UE%3d"/>
@@ -23,7 +21,10 @@ class Digiroad2TnItsApi extends ScalatraServlet {
   }
 
   get("/rosattedownload/download/readDataSet") {
-    val id = params.getOrElse("dataSetID", halt(400))
-    getServletContext.getResourceAsStream("/RosatteTestData/" + id + ".xml")
+    val id = params.getOrElse("dataSetID", halt(BadRequest("Missing mandatory parameter 'dataSetID'")))
+    Option(getServletContext.getResourceAsStream("/RosatteTestData/" + id + ".xml")).map { dataStream =>
+      contentType = "application/xml"
+      dataStream
+    }.getOrElse(NotFound("No dataset found for id " + id))
   }
 }
