8d84040f6b50d13c96f1f72c4d4b9d12a6f702a1
Mikko Hautala <mikko.hautala@reaktor.fi>
2016-03-18 15:55:07 +0200

    Return list of urls to local test datasets in queryDataSets api

--- src/main/scala/fi/liikennevirasto/digiroad2/tnits/Digiroad2TnItsApi.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/tnits/Digiroad2TnItsApi.scala
@@ -1,21 +1,32 @@
 package fi.liikennevirasto.digiroad2.tnits
 
+import java.net.URL
+import java.nio.file.FileSystems
+
 import org.scalatra._
+import collection.JavaConverters._
 
 class Digiroad2TnItsApi extends ScalatraServlet {
 
   get("/rosattedownload/download/queryDataSets") {
     contentType = "application/xml"
+
+    val dataSetPaths = getServletContext.getResourcePaths("/RosatteTestData/").asScala
+    val dataSetIds = dataSetPaths.map { _.drop("/RosatteTestData/".length).dropRight(".xml".length) }
+
     params.get("lastValidDatasetId").map { lastValidDatasetId =>
       <rst:ROSATTERestDatasetRefList xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:rst="http://www.ertico.com/en/subprojects/rosatte/rst" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
         <rst:ROSATTERestDatasetRef xlink:href="/rosattedownload/download/readDataSet?dataSetID=1uShiYqi%2fEe120s9P1ga7AAAAN2DTdVBAAAAcIdN1UE%3d"/>
       </rst:ROSATTERestDatasetRefList>
     }.getOrElse {
+      val dataSetUrls = dataSetIds.map { dataSetId =>
+        "http://localhost:8080/rosattedownload/download/readDataSet?dataSetID=" + java.net.URLEncoder.encode(dataSetId, "UTF-8")
+      }
+
       <rst:ROSATTERestDatasetRefList xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:rst="http://www.ertico.com/en/subprojects/rosatte/rst" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-        <rst:ROSATTERestDatasetRef xlink:href="/rosattedownload/download/readDataSet?dataSetID=1uShiYqi%2fEe120s9P1ga7AAAAPSdSdVBAAAAHEdL1UE%3d"/>
-        <rst:ROSATTERestDatasetRef xlink:href="/rosattedownload/download/readDataSet?dataSetID=1uShiYqi%2fEe120s9P1ga7AAAAGjrS9VBAAAA%2f39N1UE%3d"/>
-        <rst:ROSATTERestDatasetRef xlink:href="/rosattedownload/download/readDataSet?dataSetID=1uShiYqi%2fEe120s9P1ga7AAAADiDTdVBAAAA3YNN1UE%3d"/>
-        <rst:ROSATTERestDatasetRef xlink:href="/rosattedownload/download/readDataSet?dataSetID=1uShiYqi%2fEe120s9P1ga7AAAAN2DTdVBAAAAcIdN1UE%3d"/>
+        {
+        dataSetUrls.map { url => <rst:ROSATTERestDatasetRef xlink:href={ url }/> }
+        }
       </rst:ROSATTERestDatasetRefList>
     }
   }
