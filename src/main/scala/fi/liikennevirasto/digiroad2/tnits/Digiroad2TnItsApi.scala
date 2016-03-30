5995db0906b8eab674001b770556e6a1593c74cd
Mikko Hautala <mikko.hautala@reaktor.fi>
2016-03-30 14:04:25 +0300

    Remove redundant local test data based api

--- src/main/scala/fi/liikennevirasto/digiroad2/tnits/Digiroad2TnItsApi.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/tnits/Digiroad2TnItsApi.scala
@@ -13,8 +13,6 @@ class Digiroad2TnItsApi extends ScalatraServlet with FutureSupport {
   override protected implicit def executor: ExecutionContext =
     scala.concurrent.ExecutionContext.global
 
-  // From aineistot.liikennevirasto.fi
-
   get("/rosattedownload/download/queryDataSets") {
     new AsyncResult {
       val is =
@@ -55,48 +53,6 @@ class Digiroad2TnItsApi extends ScalatraServlet with FutureSupport {
         RemoteDatasets.get(id)
       }
     }
-  }
-
-  // Local test data
-
-  get("/rosattetest/download/queryDataSets") {
-    contentType = "application/xml"
-
-    val dataSetPaths =
-      getServletContext.getResourcePaths("/RosatteTestData/").asScala
-
-    val dataSetIds =
-      dataSetPaths
-        .toSeq
-        .map { _.drop("/RosatteTestData/".length).dropRight(".xml".length) }
-        .map { id => (id, Rosatte.decodeDataSetId(URLDecoder.decode(id, "UTF-8"))) }
-        .sortBy { case (_, id) => id.startDate }
-
-    val wantedDataSetIds =
-      if (params.contains("lastValidDatasetId")) {
-        val lastValidDataSetId =
-          params("lastValidDatasetId")
-        val wantedId =
-          Rosatte.decodeDataSetId(lastValidDataSetId)
-        dataSetIds
-          .filter { case (_, id) => id.startDate.isAfter(wantedId.startDate) }
-          .map(_._1)
-      } else {
-        dataSetIds
-          .map(_._1)
-      }
-
-    <rst:ROSATTERestDatasetRefList xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:rst="http://www.ertico.com/en/subprojects/rosatte/rst" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      {wantedDataSetIds.map(dataSetElement)}
-    </rst:ROSATTERestDatasetRefList>
-  }
-
-  get("/rosattetest/download/readDataSet") {
-    val id = params.getOrElse("dataSetID", halt(BadRequest("Missing mandatory parameter 'dataSetID'")))
-    Option(getServletContext.getResourceAsStream("/RosatteTestData/" + id + ".xml")).map { dataStream =>
-      contentType = "application/xml"
-      dataStream
-    }.getOrElse(NotFound("No dataset found for id " + id))
   }
 
   def dataSetElement(id: String)(implicit request: HttpServletRequest) = {
