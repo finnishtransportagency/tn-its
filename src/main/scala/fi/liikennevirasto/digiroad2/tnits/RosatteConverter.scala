1532a6688fd990cde5054fa809bbeef42058e8e5
Mikko Hautala <mikko.hautala@reaktor.fi>
2016-03-31 14:57:29 +0300

    Extract speed limit change api repsonse as geojson

--- src/main/scala/fi/liikennevirasto/digiroad2/tnits/RosatteConverter.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/tnits/RosatteConverter.scala
@@ -5,8 +5,16 @@ import dispatch._
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
+object GeoJson {
+  case class Feature(geometry: LineString, properties: Map[String, Any])
+  case class LineString(coordinates: Seq[Seq[Double]])
+}
 
 object RosatteConverter {
+  protected implicit val jsonFormats: Formats = DefaultFormats
 
   private val changesApiUrl: Req =
     (host("localhost:6666") / "digiroad" / "api" / "changes")
@@ -18,7 +26,7 @@ object RosatteConverter {
   def main(args: Array[String]) {
     val req = (changesApiUrl / "speed_limits").addQueryParameter("since", "2016-03-01T16:00")
     val contents = Await.result(Http(req OK as.String), 30.seconds)
-    println(contents)
+    val speedLimitFeatures = (parse(contents) \ "features").extract[Seq[GeoJson.Feature]]
+    println(speedLimitFeatures)
   }
-
 }
