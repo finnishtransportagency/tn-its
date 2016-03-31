19137ff6481b2f7d19c7292dd232a1298979d081
Mikko Hautala <mikko.hautala@reaktor.fi>
2016-03-31 15:49:34 +0300

    Initial convert of speed limit changes to rosatte dataset format

--- src/main/scala/fi/liikennevirasto/digiroad2/tnits/RosatteConverter.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/tnits/RosatteConverter.scala
@@ -2,6 +2,8 @@ package fi.liikennevirasto.digiroad2.tnits
 
 import dispatch.Defaults._
 import dispatch._
+import fi.liikennevirasto.digiroad2.tnits.GeoJson.Feature
+import org.joda.time.DateTime
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -27,6 +29,71 @@ object RosatteConverter {
     val req = (changesApiUrl / "speed_limits").addQueryParameter("since", "2016-03-01T16:00")
     val contents = Await.result(Http(req OK as.String), 30.seconds)
     val speedLimitFeatures = (parse(contents) \ "features").extract[Seq[GeoJson.Feature]]
-    println(speedLimitFeatures)
+    println(convertToChangeDataSet(speedLimitFeatures))
+  }
+
+  def convertToChangeDataSet(speedLimitFeatures: Seq[Feature]) = {
+    // todo: replace hardcoded dataset id
+    <rst:ROSATTESafetyFeatureDataset xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:gmd="http://www.isotc211.org/2005/gmd" xmlns:gco="http://www.isotc211.org/2005/gco" xmlns:net="urn:x-inspire:specification:gmlas:Network:3.2" xmlns:openlr="http://www.openlr.org/openlr" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:TPEG="TPEG" xmlns:rst="http://www.ertico.com/en/subprojects/rosatte/rst" xsi:schemaLocation="http://www.ertico.com/en/subprojects/rosatte/rst http://rosatte-no.triona.se/schemas/Rosatte.xsd" gml:id="i0fbf03ad-5c7a-4490-bb7c-64f95a91cb3c">
+      { speedLimitFeatures.map(featureMember) }
+      <rst:metadata>
+        <rst:datasetId>1uShiYqi/Ee120s9P1ga7AAAADiDTdVBAAAA3YNN1UE=</rst:datasetId>
+        <rst:datasetCreationTime> { DateTime.now.toString } </rst:datasetCreationTime>
+      </rst:metadata>
+      <rst:type>Update</rst:type>
+    </rst:ROSATTESafetyFeatureDataset>
+  }
+
+  def featureMember(feature: Feature) = {
+    // todo: replace hardcoded values
+    <gml:featureMember>
+      <rst:GenericSafetyFeature gml:id="i65736963-342f-4e0d-8c8e-76cb4d39387f">
+        <rst:id>
+          <rst:SafetyFeatureId>
+            <rst:providerId>SE.TrV.NVDB</rst:providerId>
+            <rst:id>{"003c4744-28c0-42ef-be3d-107bea6bf006"}</rst:id>
+          </rst:SafetyFeatureId>
+        </rst:id>
+        <rst:locationReference>
+          <rst:INSPIRELinearLocation gml:id="i9c28bb18-72b9-460f-b243-75d4bd2afc88">
+            <net:SimpleLinearReference>
+              <net:element xlink:href="SE.TrV.NVDB:LinkSequence:1000:9729"/>
+              <net:applicableDirection>inDirection</net:applicableDirection>
+              <net:fromPosition uom="meter">0</net:fromPosition>
+              <net:toPosition uom="meter">127.108815059772</net:toPosition>
+            </net:SimpleLinearReference>
+          </rst:INSPIRELinearLocation>
+        </rst:locationReference>
+        <rst:locationReference>
+          <rst:OpenLRLocationString gml:id="i2e8364eb-eb2e-4fee-a8ec-4e425ffc0866">
+            <rst:base64String>CwjjfCgGCBt5Av9MADYbCQ==</rst:base64String>
+            <rst:OpenLRBinaryVersion>1.4</rst:OpenLRBinaryVersion>
+          </rst:OpenLRLocationString>
+        </rst:locationReference>
+        <rst:validFrom>2013-12-20</rst:validFrom>
+        <rst:updateInfo>
+          <rst:UpdateInfo>
+            <rst:type>Remove</rst:type>
+          </rst:UpdateInfo>
+        </rst:updateInfo>
+        <rst:source>Regulation</rst:source>
+        <rst:encodedGeometry>
+          <gml:LineString gml:id="ibce9895a-b1b8-47be-81a4-99ea59f4cfe6" srsDimension="2">
+            <gml:posList>
+              12.4996131654393 56.2831283339562 12.4983811088529 56.2835169017528 12.4979903270137 56.2836199295346 12.4978083213852 56.2836708417839
+            </gml:posList>
+          </gml:LineString>
+        </rst:encodedGeometry>
+        <rst:type>RoadNumber</rst:type>
+        <rst:properties>
+          <rst:SafetyFeaturePropertyValue>
+            <rst:type>RoadNumber</rst:type>
+            <rst:propertyValue>
+              <gml:measure uom="">111</gml:measure>
+            </rst:propertyValue>
+          </rst:SafetyFeaturePropertyValue>
+        </rst:properties>
+      </rst:GenericSafetyFeature>
+    </gml:featureMember>
   }
 }
