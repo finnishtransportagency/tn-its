003c48e19c10f4314512c3d1124981724cb09844
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2016-04-07 15:24:20 +0300

    Convert tests to ScalaTest so we can run them

--- src/test/scala/fi/liikennevirasto/digiroad2/tnits/openlr/DigiroadFixtureSpec.scala
+++ src/test/scala/fi/liikennevirasto/digiroad2/tnits/openlr/DigiroadFixtureSpec.scala
@@ -1,21 +1,21 @@
 package fi.liikennevirasto.digiroad2.tnits.openlr
 
 import fi.liikennevirasto.digiroad2.tnits.geometry.Point
-import org.specs2.mutable.Specification
+import org.scalatest.FunSuite
 
 /**
   * Tests for DigiroadFixtureMapDatabase, DigiroadLine and DigiroadNode.
   */
-class DigiroadFixtureSpec extends Specification {
-  "Line start and endpoints" >> {
+class DigiroadFixtureSpec extends FunSuite {
+  test("Line start and endpoints") {
     val mapDatabase = DigiroadFixtureMapDatabase(Seq(
       DigiroadLine(1, Seq(Point(0, 0), Point(10, 0)), 10),
       DigiroadLine(2, Seq(Point(10, 0), Point(30, 0)), 20)
     ))
 
-    mapDatabase.getLine(1).getStartNode must_== DigiroadNode(Point(0, 0))
-    mapDatabase.getLine(1).getEndNode must_== DigiroadNode(Point(10, 0))
-    mapDatabase.getLine(2).getStartNode must_== DigiroadNode(Point(10, 0))
-    mapDatabase.getLine(2).getEndNode must_== DigiroadNode(Point(30, 0))
+    assert(mapDatabase.getLine(1).getStartNode == DigiroadNode(Point(0, 0)))
+    assert(mapDatabase.getLine(1).getEndNode == DigiroadNode(Point(10, 0)))
+    assert(mapDatabase.getLine(2).getStartNode == DigiroadNode(Point(10, 0)))
+    assert(mapDatabase.getLine(2).getEndNode == DigiroadNode(Point(30, 0)))
   }
 }
--- src/test/scala/fi/liikennevirasto/digiroad2/tnits/rosatte/RosatteSpec.scala
+++ src/test/scala/fi/liikennevirasto/digiroad2/tnits/rosatte/RosatteSpec.scala
@@ -5,10 +5,10 @@ import java.time.temporal.ChronoUnit
 import java.util.UUID
 
 import fi.liikennevirasto.digiroad2.tnits.rosatte.DatasetID.DataSetId
-import org.specs2.mutable.Specification
+import org.scalatest.FunSuite
 
-class RosatteSpec extends Specification {
-  "Data set ID values should roundtrip" >> {
+class RosatteSpec extends FunSuite {
+  test("Dataset IDs should rountrip") {
     val uuid = UUID.randomUUID()
     val start = Instant.now.minus(1, ChronoUnit.DAYS).truncatedTo(ChronoUnit.SECONDS)
     val end = Instant.now.truncatedTo(ChronoUnit.SECONDS)
@@ -16,8 +16,8 @@ class RosatteSpec extends Specification {
     val encodedDataSetId = DatasetID.encode(uuid, start, end)
     val DataSetId(decodedUuid, decodedStart, decodedEnd) = DatasetID.decode(encodedDataSetId)
 
-    decodedUuid must_== uuid
-    decodedStart must_== start
-    decodedEnd must_== end
+    assert(decodedUuid == uuid)
+    assert(decodedStart == start)
+    assert(decodedEnd == end)
   }
 }
