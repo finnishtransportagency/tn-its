894c9ac6789becf0719bb48faf58092833fdda9e
Mikko Hautala <mikko.hautala@reaktor.fi>
2016-04-13 15:09:00 +0300

    Extract method to fetch changes from OTH api

--- src/main/scala/fi/liikennevirasto/digiroad2/tnits/oth/OTHClient.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/tnits/oth/OTHClient.scala
@@ -6,6 +6,7 @@ import dispatch._
 import dispatch.Defaults._
 import fi.liikennevirasto.digiroad2.tnits.rosatte.features
 import fi.liikennevirasto.digiroad2.tnits.config
+import fi.liikennevirasto.digiroad2.tnits.rosatte.features.Asset
 import org.json4s.{DefaultFormats, Formats}
 import org.json4s.jackson.JsonMethods._
 
@@ -23,18 +24,18 @@ object OTHClient {
         password = config.logins('oth).password)
 
   def readSpeedLimitChanges(since: Instant, until: Instant): Seq[features.Asset] = {
-    val req = (changesApiUrl / "speed_limits")
-      .addQueryParameter("since", since.toString)
-      .addQueryParameter("until", until.toString)
-    val contents = Await.result(Http(req OK as.String), 30.seconds)
-    (parse(contents) \ "features").extract[Seq[features.Asset]]
+    fetchChanges("speed_limits", since, until)
   }
 
   def readTotalWeightLimitChanges(since: Instant, until: Instant): Seq[features.Asset] = {
-    val req = (changesApiUrl / "total_weight_limits")
+    fetchChanges("total_weight_limits", since, until)
+  }
+
+  private def fetchChanges(apiEndpoint: String, since: Instant, until: Instant): Seq[Asset] = {
+    val req = (changesApiUrl / apiEndpoint)
       .addQueryParameter("since", since.toString)
       .addQueryParameter("until", until.toString)
     val contents = Await.result(Http(req OK as.String), 30.seconds)
-    (parse(contents) \ "features").extract[Seq[features.Asset]]
+    (parse(contents) \ "features").extract[Seq[Asset]]
   }
 }
