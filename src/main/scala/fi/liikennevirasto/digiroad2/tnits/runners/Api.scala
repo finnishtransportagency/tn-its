7ce3067d972e0671ebb8b9c55aa4108035357063
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2016-05-17 12:57:43 +0300

    Document the Api runner

--- src/main/scala/fi/liikennevirasto/digiroad2/tnits/runners/Api.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/tnits/runners/Api.scala
@@ -5,6 +5,8 @@ import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
 
+// Read src/main/scala/ScalatraBootstrap.scala to see the Api listeners. It's not here
+// because of Scalatra reasons.
 object Api {
   def main(args: Array[String]) {
     val server = new Server(config.apiPort)
