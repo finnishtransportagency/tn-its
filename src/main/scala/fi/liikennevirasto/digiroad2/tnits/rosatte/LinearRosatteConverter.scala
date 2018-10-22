a0d1b084aa2d745fafb6cea6bd3b2ebe8f2f1e26
cardadorm <miguel.cardador@cgi.com>
2018-10-22 18:58:50 +0100

    fix unit test

--- src/main/scala/fi/liikennevirasto/digiroad2/tnits/rosatte/LinearRosatteConverter.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/tnits/rosatte/LinearRosatteConverter.scala
@@ -26,7 +26,7 @@ object LinearRosatteConverter extends AssetRosatteConverter {
     <rst:INSPIRELinearLocation gml:id={ UUID.randomUUID().toString }>
         <net:SimpleLinearReference>
           <net:element xlink:href= { DefaultLinkReference + properties.link.id } />
-          <net:applicableDirection> { applicableDirection(properties.sideCode) }</net:applicableDirection>
+          { applicableDirection(properties.sideCode) }
           <net:fromPosition uom="meter"> { properties.startMeasure }</net:fromPosition>
           <net:toPosition uom="meter"> { properties.endMeasure }</net:toPosition>
         </net:SimpleLinearReference>
