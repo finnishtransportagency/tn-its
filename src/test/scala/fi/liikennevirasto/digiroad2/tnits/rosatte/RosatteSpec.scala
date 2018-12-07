efeead94221532545222bfe71e87c309dc2b9cbd
oskarmalmstrom <oskar.malmstrom@cgi.com>
2018-12-07 16:22:57 +0200

    DROTH-507 change after master merge imports

--- src/test/scala/fi/liikennevirasto/digiroad2/tnits/rosatte/RosatteSpec.scala
+++ src/test/scala/fi/liikennevirasto/digiroad2/tnits/rosatte/RosatteSpec.scala
@@ -5,7 +5,6 @@ import java.time.Instant
 import java.time.temporal.ChronoUnit
 import java.util.UUID
 
-import fi.liikennevirasto.digiroad2.tnits.geojson.{Feature, FeatureLinear}
 import fi.liikennevirasto.digiroad2.tnits.oth.OTHClient
 import fi.liikennevirasto.digiroad2.tnits.rosatte.DatasetID.DataSetId
 import fi.liikennevirasto.digiroad2.tnits.rosatte.features.{Asset, NumericAssetProperties}
