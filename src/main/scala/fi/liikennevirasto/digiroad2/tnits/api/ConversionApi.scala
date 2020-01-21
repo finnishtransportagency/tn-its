2b8189a9631b9a1f0353c261173b9a9f4823155b
cardadorm <miguel.cardador@cgi.com>
2020-01-21 12:42:14 +0200

    Std convert limited

--- src/main/scala/fi/liikennevirasto/digiroad2/tnits/api/ConversionApi.scala
+++ src/main/scala/fi/liikennevirasto/digiroad2/tnits/api/ConversionApi.scala
@@ -1,10 +1,12 @@
 package fi.liikennevirasto.digiroad2.tnits.api
 
 import java.io.PrintWriter
-import java.time.Instant
+import java.time.format.DateTimeFormatter
+import java.time.{Instant, LocalDateTime, ZoneId}
 import java.time.temporal.ChronoUnit
+import java.util.Locale
 
-import fi.liikennevirasto.digiroad2.tnits.runners.{StdConverter, BusStopConverter, Converter, NonStdConverter}
+import fi.liikennevirasto.digiroad2.tnits.runners.{BusStopConverter, Converter, NonStdConverter, StdConverter}
 import org.scalatra._
 
 import scala.concurrent.ExecutionContext
@@ -28,18 +30,29 @@ class ConversionApi extends ScalatraServlet with FutureSupport with Authenticati
 
 
 def manual(converter: Converter, writer: PrintWriter): Unit = {
-  val startDate = converter.getLatestEndTime.getOrElse(Instant.now.minus(1, ChronoUnit.DAYS))
-  val endDate = Instant.parse(params("endDate"))
 
-  if(startDate.isAfter(endDate)  || endDate.isAfter(Instant.now))
-    halt(BadRequest(s"Wrong date period startDate - $startDate / endDate - $endDate "))
+  val startDate =  LocalDateTime.parse("08:15 PM, 01/09/2020" , DateTimeFormatter.ofPattern( "hh:mm a, M/d/uuuu" , Locale.UK )).atZone(ZoneId.of( "GMT" )).toInstant
 
-  val numberOfDays = startDate.until(endDate, ChronoUnit.DAYS)
+  for(counter <- 1 to 200)
+  converter.convert(writer, Some(startDate.plus((counter - 1) * 10 , ChronoUnit.MINUTES)), Some(startDate.plus(counter * 10, ChronoUnit.MINUTES)))
 
-  for (counter <- 1 to numberOfDays.toInt) {
-    writer.println(s"***** Convert *****")
-    converter.convert(writer, Some(startDate.plus(counter - 1, ChronoUnit.DAYS)), Some(startDate.plus(counter, ChronoUnit.DAYS)))
-  }
+  //val endDate = LocalDateTime.parse("09:15 PM, 01/09/2020" , DateTimeFormatter.ofPattern( "hh:mm a, M/d/uuuu" , Locale.UK )).atZone(ZoneId.of( "GMT" )).toInstant)
+  //        Instant.now.minus(1, ChronoUnit.MINUTES))
+
+
+
+  //val startDate = converter.getLatestEndTime.getOrElse(Instant.now.minus(1, ChronoUnit.DAYS))
+  //val endDate = Instant.parse(params("endDate"))
+
+  //if(startDate.isAfter(endDate)  || endDate.isAfter(Instant.now))
+  //  halt(BadRequest(s"Wrong date period startDate - $startDate / endDate - $endDate "))
+
+  //val numberOfDays = startDate.until(endDate, ChronoUnit.DAYS)
+
+//  for (counter <- 1 to numberOfDays.toInt) {
+//    writer.println(s"***** Convert *****")
+//    converter.convert(writer, Some(startDate.plus(counter - 1, ChronoUnit.DAYS)), Some(startDate.plus(counter, ChronoUnit.DAYS)))
+//  }
 }
 
   post("/:endDate") {
